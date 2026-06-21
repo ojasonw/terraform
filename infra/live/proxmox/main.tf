f1c2ab706882acb690694d52bc677d50db7e4566
ojasonw <jason.willian2001@gmail.com>
2026-06-21 19:44:55 -0300

    chore: change vms types

--- infra/live/proxmox/main.tf
+++ infra/live/proxmox/main.tf
@@ -35,32 +35,35 @@ locals {
   }
 
   vms = {
-    joga-together = {
-      name         = "joga-together"
+    monitoring = {
+      name         = "monitoring"
       os           = "ubuntu_2404"
       auth_mode    = "ssh_key"
-      cpu_cores    = 1
-      mem_mb       = 10240
-      disk_size_gb = 10
-    }
-
-    homelab-monitoring = {
-      name         = "homelab-monitoring"
-      os           = "ubuntu_2404"
-      auth_mode    = "ssh_key"
+      ipv4_address = "192.168.15.201"
       cpu_cores    = 2
       mem_mb       = 6144
       disk_size_gb = 60
     }
 
-    homelab-dev = {
-      name         = "homelab-dev"
+    dev = {
+      name         = "dev"
       os           = "ubuntu_2404"
       auth_mode    = "ssh_key"
+      ipv4_address = "192.168.15.202"
       cpu_cores    = 4
       mem_mb       = 8192
       disk_size_gb = 100
     }
+    homolog = {
+      name         = "homolog"
+      os           = "ubuntu_2404"
+      auth_mode    = "ssh_key
+      node         = "proxmox-homelab-b"
+      ipv4_address = "192.168.15.203"
+      cpu_cores    = 4
+      mem_mb       = 8192
+      disk_size_gb = 100
+    }    
   }
 }
 
